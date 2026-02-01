8ec8ae0e78c2f7101034f1895c65f3ee3ad4d7e1
Narendra <vaughnick@gmail.com>
2026-02-01 12:48:57 +0530

    front design set

---
+++ resources/views/front/layouts/components/header.blade.php
@@ -0,0 +1,28 @@
+<body class="text-white">
+    <nav class="navbar navbar-expand-lg navbar-dark py-3" role="navigation" aria-label="Main navigation">
+        <div class="container-lg">
+            <a class="navbar-brand fw-bold fs-4 text-gradient-primary d-flex align-items-center gap-2" href="#" aria-label="Enterprise Suite Home">
+                <span class="material-symbols-rounded">rocket_launch</span>
+                <span>Enterprise Suite</span>
+            </a>
+            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mainNav">
+                <ul class="navbar-nav ms-auto gap-3">
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-custom px-0" href="#features">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-custom px-0" href="#solutions">Solutions</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-custom px-0" href="#pricing">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-custom px-0" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
