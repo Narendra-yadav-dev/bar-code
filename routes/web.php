ba10b599061ee264ff12fdaf9e22555c924a586c
Narendra <vaughnick@gmail.com>
2026-02-11 10:34:17 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\admin\DashboardController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
